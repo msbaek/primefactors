4b49bc0faafab10cd0a7d39d752f1a30fd5c570b
msbaek <msbaek@daumcorp.com>
2013-06-21 15:01:49 +0900

    canFactorIntoPrimes - add failing test
    - add case for 2

--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -12,13 +12,14 @@ public class PrimeFactorsTest {
     @Test public void
     canFactorIntoPrimes() {
         assertEquals(list(), of(1));
+        assertEquals(list(2), of(2));
     }
 
     private List<Integer> of(int n) {
         return new ArrayList<Integer>();
     }
 
-    private List<Integer> list() {
-        return Arrays.asList();
+    private List<Integer> list(Integer ... ints) {
+        return Arrays.asList(ints);
     }
 }
