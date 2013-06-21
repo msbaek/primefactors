1dd6e0f85eaf002093f9f514da6c2be5ba795055
msbaek <msbaek@daumcorp.com>
2013-06-21 16:44:04 +0900

    canFactorIntoPrimes - refactoring for clean code
    - get rid of more braces

--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -29,10 +29,9 @@ public class PrimeFactorsTest {
     private List<Integer> of(int n) {
         ArrayList<Integer> factors = new ArrayList<Integer>();
 
-        for (int divisor = 2; n > 1; divisor++) {
+        for (int divisor = 2; n > 1; divisor++)
             for (; n % divisor == 0; n /= divisor)
                 factors.add(divisor);
-        }
         if (n > 1)
             factors.add(n);
         return factors;
