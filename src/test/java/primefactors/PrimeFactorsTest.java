622d31f6a1eb865110e318c89c92d7d4bed5ff0c
msbaek <msbaek@daumcorp.com>
2013-06-21 14:56:28 +0900

    canFactorIntoPrimes - make it work
    - resolve compile errors

--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -3,12 +3,21 @@ package primefactors;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class PrimeFactorsTest {
     @Test public void
     canFactorIntoPrimes() {
-        assertEquals(Arrays.asList(), of(1));
+        assertEquals(list(), of(1));
+    }
+
+    private List<Integer> of(int n) {
+        return null;
+    }
+
+    private List<Integer> list() {
+        return Arrays.asList();
     }
 }
