2a7a3d93bd18d8a7e13aee4d21d3623d0b3b7bd8
msbaek <msbaek@daumcorp.com>
2013-06-21 16:38:38 +0900

    canFactorIntoPrimes - refactoring for 9
    - 3에 대한 while loop 제거
    - 2를 divisor로 extract variable
    - if를 while로 변경

--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -28,15 +28,13 @@ public class PrimeFactorsTest {
 
     private List<Integer> of(int n) {
         ArrayList<Integer> factors = new ArrayList<Integer>();
-        if (n > 1) {
-            while(n % 2 == 0) {
-                factors.add(2);
-                n /= 2;
+        int divisor = 2;
+        while (n > 1) {
+            while(n % divisor == 0) {
+                factors.add(divisor);
+                n /= divisor;
             }
-            while(n % 3 == 0) {
-                factors.add(3);
-                n /= 3;
-            }
+            divisor++;
         }
         if(n > 1)
             factors.add(n);
